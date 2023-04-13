c8df6fd8197c62ca05690e8fdf9550518de82fa1
Sourabh Kothari <jainjain390@gmail.com>
2023-04-14 04:03:36 +0530

    added comments for better understanding

--- ProgWorld/src/Recursion/Backtracking/Subsets2ii.java
+++ ProgWorld/src/Recursion/Backtracking/Subsets2ii.java
@@ -1,32 +1,41 @@
 package src.Recursion.Backtracking;
-
+//https://leetcode.com/problems/subsets-ii/
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Subsets2ii {
-    private static void helper(int[] nums,int ind,List<Integer> op,List<List<Integer>> res){
+    private static void helpersubsetsWithDup(int[] nums,List<Integer> op,List<List<Integer>> res,int start){
 
         res.add(new ArrayList<>(op));
 
-
-        for(int i=ind;i<nums.length;i++){
-            if(i>ind && nums[i]==nums[i-1])
+        for(int i=start;i<nums.length;i++){
+            /*
+                we have included start element thats why we will start checking
+                from next element of start
+                if the next ele is equal to prev we will skip
+                no further recursion calls for them as they will generate
+                same answers
+            */
+            if(i>start && nums[i]==nums[i-1])
                 continue;
 
             op.add(nums[i]);
-            helper(nums,i+1,op,res);
+            helpersubsetsWithDup(nums,op,res,i+1);
             op.remove(op.size()-1);
         }
 
-
     }
     public static List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
+        List<List<Integer>> res=new ArrayList<>();
         List<Integer> op = new ArrayList<>();
-        List<List<Integer>> res = new ArrayList<>();
-
-        helper(nums,0,op,res);
+        /*
+            because we are dealing with positions so we want to skip duplicates
+            which can only be done if they all come together thats why we are sorting
+            the array
+        */
+        Arrays.sort(nums);
+        helpersubsetsWithDup(nums,op,res,0);
 
         return res;
     }
