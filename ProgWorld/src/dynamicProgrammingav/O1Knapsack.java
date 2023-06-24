fbf8205c1a389deffab9586661a11fb73a0e8530
Sourabh Kothari <jainjain390@gmail.com>
2023-06-24 17:12:39 +0530

    modified O/1 knapsack

--- ProgWorld/src/dynamicProgrammingav/O1Knapsack.java
+++ ProgWorld/src/dynamicProgrammingav/O1Knapsack.java
@@ -3,14 +3,15 @@ package src.dynamicProgrammingav;
 public class O1Knapsack {
     private static int knapSackRecursive(int w, int wt[], int val[], int n)
     {
-        if(w<=0 || n==0)
+        if(w==0 || n==0)
             return 0;
 
         //include
         if(w>=wt[n-1]){
             return Math.max(val[n-1] + knapSackRecursive(w-wt[n-1],wt,val,n-1),knapSackRecursive(w,wt,val,n-1));
         }
-
+        
+        //exclude because out of capacity
         return knapSackRecursive(w,wt,val,n-1);
     }
     private static int knapSackRecursiveMem(int w, int wt[], int val[], int n,int[][] dp)
@@ -27,7 +28,6 @@ public class O1Knapsack {
         }
 
         dp[n][w] = knapSackRecursiveMem(w,wt,val,n-1,dp);
-
         return dp[n][w];
     }
     private static int topDown(int w, int wt[], int val[], int n,int[][] dp){
@@ -39,15 +39,15 @@ public class O1Knapsack {
         }
 
         for(int i=1;i<n+1;i++){
-            for(int j=1;j<w+1;j++){
+            for(int remainingCap=1;remainingCap<w+1;remainingCap++){
                 //include
-                if(wt[i-1]<=j){
-                    dp[i][j] = Math.max(val[i-1] + dp[i-1][j-wt[i-1]],dp[i-1][j]);
+                if(wt[i-1]<=remainingCap){
+                    dp[i][remainingCap] = Math.max(val[i-1] + dp[i-1][remainingCap-wt[i-1]],dp[i-1][remainingCap]);
 
                 }
                 //exclude
                 else
-                    dp[i][j] = dp[i-1][j];
+                    dp[i][remainingCap] = dp[i-1][remainingCap];
             }
         }
         return dp[n][w];
